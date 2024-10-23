1d593c4686b9ab7499d531b28ec404ed2eeb7e43
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-23 14:24:20 +0300

    fix: Correct Resume page padding

--- src/components/resume/Right.tsx
+++ src/components/resume/Right.tsx
@@ -4,7 +4,7 @@ import WorkExperienceBlock from './WorkExperienceBlock';
 
 export default function Right() {
   return (
-    <section className="flex w-[469px] flex-col justify-between gap-4 rounded-l-lg p-6 py-[19px]">
+    <section className="flex w-[469px] flex-col justify-between gap-4 rounded-l-lg p-6 pt-[19px]">
       <WorkExperienceBlock />
       <div className="block min-h-[1px] w-full rounded-sm bg-primary" />
       <CoursesBlock />
