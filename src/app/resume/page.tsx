86ea19c8edb4e957144a2590e2d4209d9e2dc485
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-24 18:30:48 +0300

    refactor: Correct Resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,18 @@
 import BlurFade from '@/components/magicui/blur-fade';
-import ResumeLayout from '@/components/resume/ResumeLayout';
 import Link from 'next/link';
+import Info from './info';
+import Contacts from './contacts';
+import About from './about';
+import Skills from './skills';
+import Languages from './languages';
+import Courses from './courses';
+import Education from './education';
+import Experience from './experience';
+import { DATA } from '@/data/resume';
+
+function Divider() {
+  return <div className="block min-h-[1px] w-full rounded-sm bg-primary" />;
+}
 
 const BLUR_FADE_DELAY = 0.04;
 
@@ -33,7 +45,32 @@ export default async function ResumePage() {
           </div>
         </BlurFade>
         <BlurFade delay={BLUR_FADE_DELAY * 15}>
-          <ResumeLayout />
+          <main className="flex justify-center text-xs leading-[1.2]">
+            <div className="relative flex min-w-[325px] max-w-[325px] flex-col rounded-lg shadow-[5px_5px_0px_0px] outline outline-1 outline-primary md:max-h-[1122px] md:min-h-[1122px] md:min-w-[794px] md:max-w-[794px] md:flex-row">
+              <section className="flex w-[325px] flex-col justify-between rounded-l-lg bg-secondary p-6 pt-[18px]">
+                <Info />
+                <Divider />
+                <Contacts
+                  contacts={DATA.contact.social}
+                  location={DATA.location}
+                  locationLink={DATA.locationLink}
+                />
+                <Divider />
+                <About about={DATA.about} />
+                <Divider />
+                <Skills />
+                <Divider />
+                <Languages />
+              </section>
+              <section className="flex w-[469px] flex-col justify-between gap-4 rounded-l-lg p-6 pt-[19px]">
+                <Experience />
+                <Divider />
+                <Courses courses={DATA.courses} />
+                <Divider />
+                <Education />
+              </section>
+            </div>
+          </main>
         </BlurFade>
       </div>
     </main>
