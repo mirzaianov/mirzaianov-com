06450d33cc8b2a331f9c170e0aa45b07522338cf
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-28 20:11:06 +0300

    feat: Add word-pull-up animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import BlurFade from '@/components/magicui/blur-fade';
 import BlurFadeText from '@/components/magicui/blur-fade-text';
+import WordPullUp from '@/components/magicui/word-pull-up';
 import { ProjectCard } from '@/components/project-card';
 import { ResumeCard } from '@/components/resume-card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
@@ -25,11 +26,9 @@ export default async function Page() {
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="flex justify-between gap-2">
             <div className="flex flex-1 flex-col space-y-1.5">
-              <BlurFadeText
-                delay={BLUR_FADE_DELAY}
+              <WordPullUp
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-                yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(' ')[0]}`}
+                words={`Hi, I'm ${DATA.name.split(' ')[0]}`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
