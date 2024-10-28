1d14fdc3ef6d6c9838571811a9864da02ce69da7
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-28 19:53:32 +0300

    feat: Add hyper-text animation

--- src/app/resume/info.tsx
+++ src/app/resume/info.tsx
@@ -1,4 +1,5 @@
 import BlurFade from '@/components/magicui/blur-fade';
+import HyperText from '@/components/magicui/hyper-text';
 
 type Props = {
   name: string;
@@ -12,11 +13,16 @@ export default function Info({ name, role, skills }: Props) {
   return (
     <BlurFade delay={BLUR_FADE_DELAY * 12}>
       <article className="flex flex-col gap-4">
-        <h1 className="inline-block text-3xl font-bold uppercase leading-[1.1] lg:text-[40px]">
-          {name.split(' ')[0]}
-          <br />
-          {name.split(' ')[1]}
-        </h1>
+        <div className="inline-block text-3xl font-bold uppercase leading-[1.1] lg:text-[40px]">
+          <HyperText
+            text={name.split(' ')[0]}
+            duration={1500}
+          />
+          <HyperText
+            text={name.split(' ')[1]}
+            duration={1500}
+          />
+        </div>
         <div className="-mt-[6px] flex flex-col gap-1">
           <h2 className="text-base uppercase leading-[1.2] lg:text-xl lg:leading-[1.2]">
             {role}
