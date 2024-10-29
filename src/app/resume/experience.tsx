cdfa5d994b7688dd43f1769fdbadd75605f7cd2d
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-29 19:02:53 +0300

    fix: Correct responsiveness to headings

--- src/app/resume/experience.tsx
+++ src/app/resume/experience.tsx
@@ -1,4 +1,5 @@
 import BlurFade from '@/components/magicui/blur-fade';
+import { shortenDate } from '@/lib/utils';
 import Link from 'next/link';
 
 type Job = {
@@ -6,7 +7,7 @@ type Job = {
   title: string;
   href: string;
   start: string;
-  end?: string;
+  end: string;
   field: string;
   specialization: string;
   description: readonly string[];
@@ -35,18 +36,19 @@ export default function Experience({ work }: Props) {
                 className="flex flex-col gap-2"
               >
                 <div className="flex flex-col gap-1">
-                  <div className="text-base font-bold leading-[1.2]">
+                  <div className="text-sm font-bold leading-[1.2] lg:text-base lg:leading-[1.2]">
                     {job.title}
                   </div>
-                  <div className="flex flex-wrap items-center gap-1">
+                  <div className="flex flex-wrap items-center gap-1 text-sm leading-[1.2] lg:text-base lg:leading-[1.2]">
                     <Link
                       href={job.href}
-                      className="lg:link-hover-effect lg:link-hover-effect:hover lg:no-truncate truncate text-base font-bold leading-[1.2] underline underline-offset-[0.1rem] lg:self-start lg:overflow-auto lg:no-underline"
+                      className="lg:link-hover-effect lg:link-hover-effect:hover lg:no-truncate truncate font-bold underline underline-offset-[0.1rem] lg:self-start lg:overflow-auto lg:no-underline"
                     >
                       {job.company}
                     </Link>
-                    <div className="text-base leading-[1.2]">•</div>
-                    <div className="text-base leading-[1.2]">{`${job.start} - ${job.end || 'Present'}`}</div>
+                    <div className="">•</div>
+                    <div className="lg:hidden">{`${shortenDate(job.start)} - ${shortenDate(job.end) || 'Present'}`}</div>
+                    <div className="hidden lg:block">{`${job.start} - ${job.end || 'Present'}`}</div>
                   </div>
                   <div className="flex flex-wrap items-center gap-1">
                     <div>{job.field}</div>
