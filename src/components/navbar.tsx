69d7d16f27c1fc3604e8d8e3cd87f4a3b61f7ada
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-10-31 15:33:31 +0300

    refactor: Update inner shadow in Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -84,7 +84,7 @@ export default function Navbar() {
           </Tooltip>
         </DockIcon>
       </Dock>
-      <Dock className="pointer-events-auto relative z-50 mx-auto hidden h-full min-h-full transform-gpu items-center bg-background px-1 [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] sm:flex">
+      <Dock className="dark:navbar-shadow-dark-inner pointer-events-auto relative z-50 mx-auto hidden h-full min-h-full transform-gpu items-center bg-background px-1 [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] dark:[border:1px_solid_rgba(255,255,255,.1)] sm:flex">
         {MAIN_DATA.navbar.map((item) => (
           <DockIcon key={item.href}>
             <Tooltip>
