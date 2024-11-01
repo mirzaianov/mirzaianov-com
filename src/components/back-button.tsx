a6a20036f2c3cfe25fedea4f299013984f080fd5
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-11-01 16:20:06 +0300

    feat: Add animation to back-button

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,16 +1,23 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 export default function BackButton() {
   const router = useRouter();
 
   return (
-    <button
+    <Button
       onClick={() => router.back()}
-      className="m-0 appearance-none border-none bg-transparent p-0 text-gray-900 underline dark:text-white"
+      variant="secondary"
+      className="group gap-2 transition-all duration-300 hover:scale-105 hover:bg-primary/20 sm:text-base sm:leading-[1.2]"
     >
-      Back
-    </button>
+      <ArrowLeft
+        size={16}
+        className="transition-transform duration-300 group-hover:-translate-x-0.5"
+      />
+      <p>Go back</p>
+    </Button>
   );
 }
